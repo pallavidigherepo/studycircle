0f57fc959cf015252d5be283cfa7ea92f8517635
Pallavi Dighe <pals.ver@gmail.com>
2022-03-15 22:18:42 +0530

    Some minors

--- meritest/vue/src/main.js
+++ meritest/vue/src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 
 import globalComponents from './global_components.js'
-
+import "./assets/app.js";
 import "./assets/app.css";
-//import "./assets/app.js";
+
 
 const app = createApp(App)
 .use(router)
